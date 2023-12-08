7044cf8cf2a03cd03378ea52fff0f1461ff23d56
Amey Pankanti <apankanti@paktolus.com>
2023-12-08 16:11:17 +0530

    Merging main to master

--- src/Components/SubmitDetails/SubmitDetails.tsx
+++ src/Components/SubmitDetails/SubmitDetails.tsx
@@ -30,15 +30,13 @@ const navigate = useNavigate();
 
   const submitButtonHandler = async () => {
     try {
-      const bearerToken: string =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImJvYkBwYWt0b2x1cy5jb20iLCJuYW1laWQiOiJhYTgzOTQyNS00OWU4LTQwNzItOGJlNC0yMDQ5MDU4YzYwOGQiLCJnaXZlbl9uYW1lIjoiQm9iIiwiZmFtaWx5X25hbWUiOiJBbmFzb3JpIiwidXNlcl92aWV3X2FjY2Vzc190eXBlIjoiRGVhbGVyIiwicm9sZSI6WyJEZWFsZXIiLCJWaWV3IEludm9pY2UiLCJTYXZlIFF1b3RlcyIsIkVkaXQgTWFya3VwIiwiVmlldyBSZW1pdHRhbmNlIiwiTWFuYWdlIEFDSCBJbmZvcm1hdGlvbiIsIkNhbmNlbCBDb250cmFjdCIsIlZvaWQgQ29udHJhY3RzIiwiVmlldyBSZXRhaWwgUHJpY2UgTGltaXRzIiwiRWRpdCBSZXRhaWwgUHJpY2UgTGltaXRzIiwiUGF5IFJlbWl0dGFuY2UiLCJFbnRlciBDb250cmFjdCIsIlByb2Nlc3MgUXVvdGVzIiwiU2FsZXMgQXNzb2NpYXRlcyIsIlZpZXcgTWFya3VwIl0sInNpZ25faW4iOiJtZmEiLCJuYmYiOjE2OTgwNTQ1MjYsImV4cCI6MTY5ODE0MDkyNiwiaWF0IjoxNjk4MDU0NTI2LCJpc3MiOiJodHRwczovL3pvb20tc3RhZ2UuYWxwaGF3YXJyYW50eS5jb20iLCJhdWQiOiJodHRwczovL3pvb20tc3RhZ2UuYWxwaGF3YXJyYW50eS5jb20ifQ._6DP-RHaxKa0PYP4DSIZLkJYYggfcHNGm68VMggWV9E";
-
+     
       const apiUrl: string =
-        "https://aw-test.free.beeceptor.com/api/v1/lead/save";
+        "https://localhost/3001/user/create";
 
       const headers = {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${bearerToken}`,
+        
       };
 
       const apiFormData = {
@@ -48,12 +46,12 @@ const navigate = useNavigate();
         companyName: formData.companyName,
         selectedService: formData.selectedService,
         projectBudget: formData.projectBudget,
-        requested_at: new Date().toISOString(),
+        created_at: new Date().toISOString(),
       };
 
       const response = await fetch(apiUrl, {
         method: "POST",
-        headers: headers,
+        // headers: headers,
         body: JSON.stringify(apiFormData),
       });
 
